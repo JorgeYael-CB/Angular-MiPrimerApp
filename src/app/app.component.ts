e1f5ed3fb2d92b22cbd8ff934bd0b8b6b678f6b7
JorgeYael-CB <jorge.yael333@gmail.com>
2024-08-08 20:09:35 -0600

    First commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+})
+export class AppComponent {
+  public counter:number = 10;
+  public title:string = 'Hello World.';
+}
